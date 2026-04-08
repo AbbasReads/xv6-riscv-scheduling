124fb6cc5a39e7137fd79c4cef941955a78d69e2
Yashwanth Mettu <yashwanthme2@gmail.com>
2026-04-09 00:47:31 +0530

    Created msgqueue.h file and modified proc.h file.

--- kernel/proc.h
+++ kernel/proc.h
@@ -1,3 +1,4 @@
+#include "msgqueue.h"
 // Saved registers for kernel context switches.
 struct context {
   uint64 ra;
@@ -104,4 +105,6 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
+  
+  struct msgqueue mq;
 };
