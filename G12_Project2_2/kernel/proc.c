ea94ce733dc46154416fe04fe2cddf11413f0504
Meghana-Merla <24je0654@iitism.ac.in>
2026-04-11 22:06:04 +0000

    implemented EDF scheduler in proc.c

--- G12_Project2_2/kernel/proc.c
+++ G12_Project2_2/kernel/proc.c
@@ -123,7 +123,9 @@ allocproc(void)
 
 found:
   p->pid = allocpid();
-  p->state = USED;
+p->deadline = DEFAULT_DEADLINE;
+p->arrivaltime = ticks;  
+p->state = USED;
 
   // Allocate a trapframe page.
   if((p->trapframe = (struct trapframe *)kalloc()) == 0){
@@ -425,43 +427,41 @@ void
 scheduler(void)
 {
   struct proc *p;
+  struct proc *earliest;
   struct cpu *c = mycpu();
 
   c->proc = 0;
   for(;;){
-    // The most recent process to run may have had interrupts
-    // turned off; enable them to avoid a deadlock if all
-    // processes are waiting. Then turn them back off
-    // to avoid a possible race between an interrupt
-    // and wfi.
     intr_on();
-    intr_off();
 
-    int found = 0;
-    for(p = proc; p < &proc[NPROC]; p++) {
+    earliest = 0;
+
+    // Find process with earliest deadline
+    for(p = proc; p < &proc[NPROC]; p++){
       acquire(&p->lock);
-      if(p->state == RUNNABLE) {
-        // Switch to chosen process.  It is the process's job
-        // to release its lock and then reacquire it
-        // before jumping back to us.
-        p->state = RUNNING;
-        c->proc = p;
-        swtch(&c->context, &p->context);
-
-        // Process is done running for now.
-        // It should have changed its p->state before coming back.
-        c->proc = 0;
-        found = 1;
+      if(p->state == RUNNABLE){
+        if(earliest == 0 || p->deadline < earliest->deadline){
+          if(earliest != 0)
+            release(&earliest->lock);
+          earliest = p;
+        } else {
+          release(&p->lock);
+        }
+      } else {
+        release(&p->lock);
       }
-      release(&p->lock);
     }
-    if(found == 0) {
-      // nothing to run; stop running on this core until an interrupt.
-      asm volatile("wfi");
+
+    // Run earliest deadline process
+    if(earliest != 0){
+      earliest->state = RUNNING;
+      c->proc = earliest;
+      swtch(&c->context, &earliest->context);
+      c->proc = 0;
+      release(&earliest->lock);
     }
   }
 }
-
 // Switch to scheduler.  Must hold only p->lock
 // and have changed proc->state. Saves and restores
 // intena because intena is a property of this
