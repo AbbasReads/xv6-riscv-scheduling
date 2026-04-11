074d5a2f7e8b15b520b6a8d3e8f5b0c6b92a1072
Yashwanth Mettu <yashwanthme2@gmail.com>
2026-04-12 01:56:19 +0530

    Modify scheduler to implement RMS priority based scheduling

--- G12_Project2_2/kernel/proc.c
+++ G12_Project2_2/kernel/proc.c
@@ -439,28 +439,28 @@ scheduler(void)
     intr_on();
     intr_off();
 
-    int found = 0;
-    for(p = proc; p < &proc[NPROC]; p++) {
-      acquire(&p->lock);
-      if(p->state == RUNNABLE) {
-        // Switch to chosen process.  It is the process's job
-        // to release its lock and then reacquire it
-        // before jumping back to us.
-        p->state = RUNNING;
-        c->proc = p;
-        swtch(&c->context, &p->context);
-
-        // Process is done running for now.
-        // It should have changed its p->state before coming back.
-        c->proc = 0;
-        found = 1;
-      }
-      release(&p->lock);
+    struct proc *chosen = 0;
+for(p = proc; p < &proc[NPROC]; p++) {
+    acquire(&p->lock);
+    if(p->state == RUNNABLE) {
+        if(chosen == 0 || p->priority > chosen->priority) {
+            if(chosen != 0)
+                release(&chosen->lock);
+            chosen = p;
+        } else {
+            release(&p->lock);
+        }
+    } else {
+        release(&p->lock);
     }
-    if(found == 0) {
-      // nothing to run; stop running on this core until an interrupt.
-      asm volatile("wfi");
-    }
+}
+if(chosen != 0) {
+    chosen->state = RUNNING;
+    c->proc = chosen;
+    swtch(&c->context, &chosen->context);
+    c->proc = 0;
+    release(&chosen->lock);
+}
   }
 }
 
